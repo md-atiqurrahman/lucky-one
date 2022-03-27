c88b6e81227b110359635ac7f7430867f23e4f40
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-03-27 22:22:32 +0600

    add cart

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -21,7 +21,12 @@ const Shop = () => {
               }
             </div>
             <div className="cart-container">
-                  <h2>this is cart container</h2>
+                  <h2 className='cart-title'>Selected Phones</h2>
+                  <div className='cart-btn'>
+                  <button className='btn1'><p>CHOOSE 1 FOR ME</p></button>
+                  <br/>
+                  <button className='btn2'><p>CHOOSE AGAIN</p></button>
+                  </div>
             </div>
         </div>
     );
